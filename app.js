3cf4dac711ce2b297b40d3b9b5587fdf841727ba
Miguel Pacheco <miguelwebdev765@gmail.com>
2021-12-13 17:16:38 -0600

    add fixed navbar functionality

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ const navLink = document.querySelectorAll(".nav-link");
 navToggle.addEventListener("click", function (e) {
   mainNav.classList.toggle("active");
 });
+
+window.addEventListener("scroll", function (e) {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = mainNav.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    mainNav.classList.add("sticky");
+  } else {
+    mainNav.classList.remove("sticky");
+  }
+});
