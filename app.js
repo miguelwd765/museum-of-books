9568b9df25e6e3dd2e3c204f3d3d9d3f656fcfe5
Miguel Pacheco <miguelwebdev765@gmail.com>
2021-12-14 13:15:44 -0600

    close nav and remove active class when nav link is clicked

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const mainNav = document.querySelector("#mainNavbar");
 const navToggle = document.querySelector(".navbar-toggler");
 const navLink = document.querySelectorAll(".nav-link");
 
+const navContent = document.querySelector("#navbarContent");
+const navCollapse = new bootstrap.Collapse(navContent, { toggle: false });
+
+let isNavOpen = mainNav.classList.contains("active");
+
 navToggle.addEventListener("click", function (e) {
   mainNav.classList.toggle("off-active");
   mainNav.classList.toggle("active");
+  console.log(isNavOpen);
 });
 
 window.addEventListener("scroll", function (e) {
@@ -16,3 +22,11 @@ window.addEventListener("scroll", function (e) {
     mainNav.classList.remove("sticky");
   }
 });
+
+for (let link of navLink) {
+  link.addEventListener("click", function (e) {
+    navCollapse.toggle();
+    mainNav.classList.remove("active");
+    mainNav.classList.add("off-active");
+  });
+}
